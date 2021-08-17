d46c3491255c3384a4aa3910e616e45a04733e23
Tunji <54449991+Adetunjicode@users.noreply.github.com>
2021-08-17 10:57:12 +0100

    created an admin route and made sme changes in authcontroller

---
+++ src/controllers/authController.js
@@ -0,0 +1,5 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const SuperAdmin = require('./../models/superAdminModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
