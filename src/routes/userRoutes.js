75136c2d1a51832478c17de137b8332f2e676c97
cykic <cyb3rcykic@gmail.com>
2021-08-19 10:19:50 +0000

    Feat: finished

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,7 +7,7 @@ router.route('/signup').post(userController.createUser);
 router.route('/verify').post(userController.verifyUser);
 
 router.use(authController.protected);
-
+// Protect update
 router.route('/update').post(userController.setProfilePic);
 
 module.exports = router;
