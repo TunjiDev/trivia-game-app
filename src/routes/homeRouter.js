e2b06c89fbbca9f9d60638db45fbd2367711c9d9
cykic <cyb3rcykic@gmail.com>
2021-08-19 10:35:12 +0000

    Feat: Routing to homepage

---
+++ src/routes/homeRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const catchAsync = require('../error/catchAsync');
+
+const router = express.Router();
+
+router.all(
+  '/',
+  catchAsync(async (req, res, next) => {
+    res.redirect(
+      'https://documenter.getpostman.com/view/15594941/TzzAMcSF#434e804b-831b-41e5-b48e-74ca6109b04b'
+    );
+  })
+);
+
+module.exports = router;
